5598699698c0a72039a4d68a6b3154a60bd23fd1
moalwnfkxviwbdksviasbabwbsjbazsjbsissb <130096000+moalwnfkxviwbdksviasbabwbsjbazsjbsissb@users.noreply.github.com>
2023-08-09 03:47:04 +0300

    Update env.py

--- env.py
+++ env.py
@@ -3,11 +3,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = os.getenv("API_ID", "").strip()
-API_HASH = os.getenv("API_HASH", "").strip()
-BOT_TOKEN = os.getenv("BOT_TOKEN", "").strip()
-DATABASE_URL = os.getenv("DATABASE_URL", "").strip()
-MUST_JOIN = os.getenv("MUST_JOIN", "")
+API_ID = os.getenv("API_ID", "26286754").strip()
+API_HASH = os.getenv("API_HASH", "7a637e05958e14496e6ab1910a9eb9e7").strip()
+BOT_TOKEN = os.getenv("BOT_TOKEN", "6484547301:AAEBttmjmTL42Sdz_omIJSuwx2FT2VAoWac").strip()
+DATABASE_URL = os.getenv("DATABASE_URL", "mongodb+srv://levi303:levi303@cluster0.ws4aofd.mongodb.net/?retryWrites=true&w=majority").strip()
+MUST_JOIN = os.getenv("MUST_JOIN", "-1001825940899")
 
 if not API_ID:
     print("No API_ID found. Exiting...")
